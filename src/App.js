df6c91d21311a3b3b98ea801b8bf2488a1802a57
park-go <73177125+park-go@users.noreply.github.com>
2020-11-28 17:04:11 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
 
+const customers=[
+    {
+      'id': 1,
+      'image': 'https://placeimg.com/64/64/1',
+      'name': '홍길동',
+      'birthday': '951122',
+      'gender':'남자',
+      'job': '학생'
+    },
+    {
+      'id': 2,
+      'image': 'https://placeimg.com/64/64/2',
+      'name': '장영실',
+      'birthday': '900122',
+      'gender':'여자',
+      'job': '프로그래머'
+    },
+    {
+      'id': 3,
+      'image': 'https://placeimg.com/64/64/3',
+      'name': '이순신',
+      'birthday': '821002',
+      'gender':'여자',
+      'job': '작가'
+    }
+]
 
 class App extends Component {
   render () {
     return (
-      <div className="gray-background">
-        <img src={logo} lat="logo" />
-          <h2>play!!!!!!!!!!!!!!!!!!!</h2>
+      <div>
+          {
+            customers.map(c=>{
+              return (
+                <Customer
+                  key={c.id}
+                  id={c.id}
+                  image={c.image}
+                  name={c.name}
+                  birthday={c.birthday}
+                  gender={c.gender}
+                  job={c.job}
+                />
+              )
+            })
+          }
       </div>
     );
   }
